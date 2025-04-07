dbb43625251bf3e48802c255cc0c87c8ed5f647f
yeasinshamimdev <yeasinshamim7@gamil.com>
2025-04-07 05:22:43 +0200

    arithmetic operator file added

---
+++ arithmetic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+  float a = 15;
+  int b = 2;
+  // int sum = a + b;
+  float div = a / b;
+  bool sign = true;
+  printf("%d", sign);
+  // printf("The division of %.0f and %d is: %.1f\n", a, b, div);
+
+  // printf("The sum of %d and %d is: %d\n", a, b, sum);
+
+  return 0;
+}
